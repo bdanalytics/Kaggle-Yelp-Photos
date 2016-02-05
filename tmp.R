c4e898d693b09276b50e72f9d5071cfed0288876
bdanalytics <bbalaji8@gmail.com>
2016-02-05 15:15:40 -0500

    Added resXY features

--- tmp.R
+++ tmp.R
@@ -6,3 +6,8 @@ for (feat in c("good_for_lunch", "good_for_dinner")) {
     print(sprintf("Summary for %s:", feat))
     print(table(glbObsAll$.src, glbObsAll[, feat], useNA = "ifany"))
 }
+
+myplot_violin(glbObsTrn, "resXY.mad", glb_rsp_var)
+myplot_violin(glbObsTrn, "resXY.mean", glb_rsp_var)
+glbObsTrn$resXY.mean.log1p <- log1p(glbObsTrn$resXY.mean)
+myplot_violin(glbObsTrn, "resXY.mean.log1p", glb_rsp_var)
