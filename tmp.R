6eb7885e9baa863414f5925a7f641bb150a92319
bdanalytics <bbalaji8@gmail.com>
2016-02-08 14:58:32 -0500

    Extracted & modeled color features

--- tmp.R
+++ tmp.R
@@ -11,3 +11,19 @@ myplot_violin(glbObsTrn, "resXY.mad", glb_rsp_var)
 myplot_violin(glbObsTrn, "resXY.mean", glb_rsp_var)
 glbObsTrn$resXY.mean.log1p <- log1p(glbObsTrn$resXY.mean)
 myplot_violin(glbObsTrn, "resXY.mean.log1p", glb_rsp_var)
+
+print(myplotImg(img <- readJPEG("data/photos/426819.jpg")))
+print(img[1:5, 1:5, 1])
+print(img[1:5, 1:5, 2])
+proxy::simil(x = as.vector(img[,,1]), y = as.vector(img[,,2]), method = "cosine")
+proxy::simil(x = as.vector(img[,,1])[1:5], y = as.vector(img[,,2])[1:5], method = "cosine")
+xy5 = rbind(as.vector(img[,,1])[1:5], as.vector(img[,,2])[1:5])
+as.vector(proxy::simil(rbind(as.vector(img[,,1])[1:5], as.vector(img[,,2])[1:5]), method = "cosine"))
+as.vector(proxy::simil(rbind(as.vector(img[,,1]), as.vector(img[,,2])), method = "cosine"))
+
+startTm = proc.time()["elapsed"]
+tst_vctr = feats_vctr[1:21]
+print(dsp_numeric_feats_dstrb(tst_vctr))
+print(sprintf("Elapsed seconds: %0.4f", proc.time()["elapsed"] - startTm))
+
+dplyr::arrange(glb_feats_df[, c("id", "shapiro.test.p.value")], id, shapiro.test.p.value)
